065a0dd08475410f07a9ff1b6ce3a76089ff418c
_rs <rs.on.the.internet@gmail.com>
2023-03-16 15:19:18 +0200

    Update options.lua

--- options.lua
+++ options.lua
@@ -2,10 +2,10 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
+    relativenumber = false, -- sets vim.opt.relativenumber
+    number = false, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    signcolumn = "yes", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
   },
   g = {
