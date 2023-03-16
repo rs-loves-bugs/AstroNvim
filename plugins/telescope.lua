b27a0a7916e27bca2a7a5802da15c5a08709d1e1
_rs <rs.on.the.internet@gmail.com>
2023-03-16 20:35:28 +0000

    updates

---
+++ plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      local actions = require "telescope.actions"
+      local get_icon = require("astronvim.utils").get_icon
+      return {
+        defaults = {
+          prompt_prefix = string.format("%s ", get_icon "Search"),
+          selection_caret = string.format("%s ", get_icon "Selected"),
+          path_display = { "truncate" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+  
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<esc>"] = actions.close,
+            },
+            n = { ["q"] = actions.close },
+          },
+        },
+      }
+    end
+  }
+  
