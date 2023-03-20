a4c4b563a6c9f3d8b826dbca259745ec11dc8189
_rs <rs.on.the.internet@gmail.com>
2023-03-20 11:50:00 +0200

    Update core.lua

--- plugins/core.lua
+++ plugins/core.lua
@@ -21,7 +21,13 @@ return {
 		end,
 	},
 	-- You can disable default plugins as follows:
-	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
+	{
+	"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			show_current_context = false,
+			show_current_context_start = false,
+		},
+	},
 
 	{
 		"nvim-neo-tree/neo-tree.nvim",
